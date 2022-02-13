8bd64af2ca2d5f06e83767f523434f3c801dd40e
tgalen <tgalen84@gmail.com>
2022-02-13 15:13:20 -0500

    Creat <ul> in DOM w/ person profile

--- script.js
+++ script.js
@@ -8,18 +8,29 @@ fetch("https://randomuser.me/api")
         let image = document.createElement("img");
         let mainDiv = document.getElementById("main");
         let personInfo = document.createElement("ul");
-        let li = document.createElement("li");
-        personInfo.appendChild(document.createTextNode(`Name: ${personProfile.name.first} ${personProfile.name.last}`));
-        personInfo.appendChild(document.createTextNode(`Age: ${personProfile.dob.age}`));
-        personInfo.appendChild(document.createTextNode(`Gender: ${personProfile.gender}`));
-        personInfo.appendChild(document.createTextNode(`Country: ${personProfile.location.country}`));
-        personInfo.appendChild(document.createTextNode(`City: ${personProfile.location.city}`));
-        personInfo.appendChild(document.createTextNode(`Email: ${personProfile.email}`));
+        let liName = document.createElement("li");
+        let liAge = document.createElement("li");
+        let liGender = document.createElement("li");
+        let liCountry = document.createElement("li");
+        let liCity = document.createElement("li");
+        let liEmail = document.createElement("li");
+        liName.appendChild(document.createTextNode(`Name: ${personProfile.name.first} ${personProfile.name.last}`));
+        liAge.appendChild(document.createTextNode(`Age: ${personProfile.dob.age}`));
+        liGender.appendChild(document.createTextNode(`Gender: ${personProfile.gender}`));
+        liCountry.appendChild(document.createTextNode(`Country: ${personProfile.location.country}`));
+        liCity.appendChild(document.createTextNode(`City: ${personProfile.location.city}`));
+        liEmail.appendChild(document.createTextNode(`Email: ${personProfile.email}`));
 
         image.setAttribute("id", "personPic");
         image.src = personProfile.picture.large;
         mainDiv.appendChild(image);
         mainDiv.appendChild(personInfo);
+        personInfo.appendChild(liName);
+        personInfo.appendChild(liAge);
+        personInfo.appendChild(liGender);
+        personInfo.appendChild(liCountry);
+        personInfo.appendChild(liCity);
+        personInfo.appendChild(liEmail);
     })
     .catch((error) => {
         console.log(error);
